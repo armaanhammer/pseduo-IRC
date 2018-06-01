c1b249eeadbf434312694b763f02467897657f92
armaanhammer <armaan@gmail.com>
2018-05-31 22:41:13 -0700

    Working client-server scratch code
    Code implemented using sockets, with a keyboard interrupt on the server side.

---
+++ scratch/tmc.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# simple illustration client/server pair; client program sends a string
+# to server, which echoes it back to the client (in multiple copies),
+# and the latter prints to the screen
+
+# this is the client
+
+import argparse
+import socket
+import sys
+
+def get_args():
+    parser = argparse.ArgumentParser(description='IRC_server.')
+
+    parser.add_argument(
+            '--host',
+            default='127.0.0.1',
+            help='host ip of the IRC_server')
+
+    parser.add_argument(
+            '--port',
+            default=5000,
+            type=int,
+            help='port used to communicate to the IRC_server')
+
+    parser.add_argument(
+            '-c',
+            '--config_file',
+            default='config_client.yaml',
+            help='IRC_client configuration file')
+
+    return parser.parse_args()
+
+# create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print (sys.argv[0])
+
+# connect to server
+host = sys.argv[1] # server address
+port = int(sys.argv[2]) # server port
+s.connect((host, port))
+
+x = 5
+
+while(x>0):
+  z = input()
+  print (z)
+  
+  s.sendall(bytes(z, 'utf8')) # send test string
+  #s.sendall(bytes(sys.argv[3], 'utf8')) # send test string
+  
+  print ('got here')
+  print (sys.argv[3])
+  print (sys.argv[3].encode)
+  
+  data = s.recv(100000)
+  print (data)
+  
+# =============================================================================
+#   # read echo
+#   i = 0
+#   while(1):
+#     data = s.recv(1000000) # read up to 1000000 bytes
+#     i += 1
+#     if (i < 5):
+#         print (data)
+#     if not data: # if end of data, leave loop
+#       break
+#   print ('received', len(data), 'bytes')
+# =============================================================================
+  x = x-1
+
+# close the connection
+s.close()
---
+++ scratch/tms.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+# simple illustration client/server pair; client program sends a string
+# to server, which echoes it back to the client (in multiple copies),
+# and the latter prints to the screen
+
+# this is the server
+
+import argparse
+import socket
+import sys
+
+print ('script started')
+stored_exception=None
+
+def get_args():
+    parser = argparse.ArgumentParser(description='IRC_server.')
+
+    parser.add_argument(
+            '--host',
+            default='192.168.0.108',
+            help='host ip of the IRC_server')
+
+    parser.add_argument(
+            '--port',
+            default=5000,
+            type=int,
+            help='port used to communicate to the IRC_server')
+
+    parser.add_argument(
+            '-c',
+            '--config_file',
+            default='config_server.yaml',
+            help='IRC_server configuration file')
+
+    return parser.parse_args()
+
+# create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# associate the socket with a port
+host = '' # can leave this blank on the server side
+port = int(sys.argv[1])
+s.bind((host, port))
+
+# accept "call" from client
+s.listen(1)
+conn, addr = s.accept()
+print ('client is at', addr)
+
+# read string from client (assumed here to be so short that one call to
+# recv() is enough), and make multiple copies (to show the need for the
+# "while" loop on the client side)
+
+
+chars_to_send = 1 # meaning True for initial case
+while(chars_to_send):
+  print('starting while loop')
+
+  if stored_exception:
+    print('caught exception: Keyboard Interrupt')
+    break
+    
+  try:
+    
+    data = conn.recv(1000000)
+    print (data.decode("utf-8"))
+
+# wait for the go-ahead signal from the keyboard (shows that recv() at
+# the client will block until server sends)
+# z = input()
+
+# now send
+    chars_to_send = conn.send(data)
+    print('chars_to_send')
+    print(chars_to_send)
+    
+  except KeyboardInterrupt:
+    print('do I get here or not?')
+    stored_exception=sys.exc_info()
+    print('stored_exception')
+    print(stored_exception)
+    
+# =============================================================================
+#   data = conn.recv(1000000)
+#   print (data.decode("utf-8"))
+#   conn.send(data)
+#   print ('sent data')
+# =============================================================================
+  
+
+# close the connection
+conn.close()
+print('Connection Closed')
